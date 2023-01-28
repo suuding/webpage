e0c219eccf992aea44c10e531805e7c4e3d7b90e
suuding <tngus4789@naver.com>
2023-01-28 16:13:21 +0900

    controller, service 클래스와, dto 클래스 생성

---
+++ src/main/java/com/suuding/springboot/service/posts/PostsService.java
@@ -0,0 +1,20 @@
+package com.suuding.springboot.service.posts;
+
+import com.suuding.springboot.domain.posts.PostsRepository;
+import com.suuding.springboot.web.Dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
