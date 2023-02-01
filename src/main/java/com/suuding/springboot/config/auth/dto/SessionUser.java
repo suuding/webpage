09e472f724ef75e6c5a6d05093ad6667557fa734
suuding <tngus4789@naver.com>
2023-02-01 14:19:16 +0900

    구글 로그인 기능 구현

---
+++ src/main/java/com/suuding/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,18 @@
+package com.suuding.springboot.config.auth.dto;
+
+import com.suuding.springboot.domain.user.User;
+import lombok.Getter;
+
+@Getter
+public class SessionUser {
+
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
