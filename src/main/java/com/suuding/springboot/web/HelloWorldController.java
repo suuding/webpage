2ae646b7c0fecedaabc38afd1ee451c242b29231
suuding <tngus4789@naver.com>
2023-02-06 16:59:42 +0900

    리액트와 spring 연동

---
+++ src/main/java/com/suuding/springboot/web/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.suuding.springboot.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/api/hello")
+    public String test() {
+        return "Hello, world!";
+    }
+}
