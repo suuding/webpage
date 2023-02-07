e167feda85ad4a2d9a8266ee4eed0eece15d385c
suuding <tngus4789@naver.com>
2023-02-07 21:43:05 +0900

    코드 전체 수정

--- src/main/resources/static/js.app/index.js
+++ src/main/resources/static/js.app/index.js
@@ -1,13 +1,14 @@
 var main = {
     init : function () {
         var _this = this;
-
         $('#btn-save').on('click', function () {
             _this.save();
         });
-        $('#btn-update').on('click', function() {
+
+        $('#btn-update').on('click', function () {
             _this.update();
         });
+
         $('#btn-delete').on('click', function () {
             _this.delete();
         });
@@ -32,42 +33,43 @@ var main = {
             alert(JSON.stringify(error));
         });
     },
-    update : function() {
+    update : function () {
         var data = {
             title: $('#title').val(),
             content: $('#content').val()
         };
 
-        var id=$('#id').val();
+        var id = $('#id').val();
 
         $.ajax({
             type: 'PUT',
             url: '/api/v1/posts/'+id,
             dataType: 'json',
-            contentType: 'application/json; charset=utf-8',
+            contentType:'application/json; charset=utf-8',
             data: JSON.stringify(data)
         }).done(function() {
-            alert('글이 수정되었습니다!');
-            window.location.href = "/";
-        }).fail(function () {
+            alert('글이 수정되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
             alert(JSON.stringify(error));
         });
     },
-    delete: function () {
+    delete : function () {
         var id = $('#id').val();
 
         $.ajax({
             type: 'DELETE',
             url: '/api/v1/posts/'+id,
             dataType: 'json',
-            contentType: 'application/json; charset=utf-8'
-        }).done(function () {
-            alert('글이 삭제되었습니당~');
-            window.location.href='/';
-        }).fail(function () {
+            contentType:'application/json; charset=utf-8'
+        }).done(function() {
+            alert('글이 삭제되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
             alert(JSON.stringify(error));
         });
     }
+
 };
 
 main.init();
